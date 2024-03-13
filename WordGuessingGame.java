f753ae75cc611d92d56ad7aeb82e31b36fdaaa29
Miguel Certã <112079507+mpcsource@users.noreply.github.com>
2024-03-13 09:43:19 +0000

    Nível 2

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -10,11 +10,13 @@ public class WordGuessingGame
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader reader;
     
     public WordGuessingGame(){
         hiddenWord = "abc";
         guessedWord = "___";
-        numberOfTries = -1;
+        numberOfTries = 0;
+        reader = new InputReader();
     }
     public String getHiddenWord(){
         return hiddenWord;
@@ -27,5 +29,30 @@ public class WordGuessingGame
     }
     public void showGuessedWord(){
         System.out.println(guessedWord);
+    }
+    
+    public void showWelcome(){
+        System.out.println("Olá! Bem vindo ao jogo de adivinhar a palavra. É tipo um jogo da forca.");
+    }
+    
+    public void guess(){
+        char guess = reader.getChar("Introduz uma letra!: ");
+        
+        for(int i = 0; i<hiddenWord.length(); i++){
+            if(hiddenWord.charAt(i) == guess){
+                guessedWord = guessedWord.substring(0, i)+guess+guessedWord.substring(i+1);
+                showGuessedWord();
+            }
+        }
+        
+        numberOfTries++;
+    }
+    
+    public void play(){
+        showWelcome();
+        
+        do { guess(); } while(guessedWord.trim().equals(hiddenWord.trim()) == false);
+        
+        System.out.println("Correto! A palavra era " + hiddenWord + "com " + getNumberOfTries() + "tentativas.");
     }
 }
