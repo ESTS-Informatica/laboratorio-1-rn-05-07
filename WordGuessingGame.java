722f08b6c9896a69675d565c14f6b816b45b9345
diox0843 <153206798+diox0843@users.noreply.github.com>
2024-03-13 08:56:28 +0000

    Create WordGuessingGame.java

---
+++ WordGuessingGame.java
@@ -0,0 +1,19 @@
+
+/**
+ * Escreva uma descrição da classe WordGuessingGame aqui.
+ * 
+ * @author (seu nome) 
+ * @version (um número da versão ou uma data)
+ */
+public class WordGuessingGame
+{
+    private String hiddenWord;
+    private String guessedWord;
+    private int numberOfTries;
+    
+    public WordGuessingGame(){
+        hiddenWord = "abc";
+        guessedWord = "___";
+        numberOfTries = -1;
+    }
+}
